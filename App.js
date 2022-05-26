42390bbb8184549fd4c4bbb1972b4136ae90bf94
Mushawiruddin <angkomuhtar@gmail.com>
2022-05-26 21:37:21 +0800

    first

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text, SafeAreaView} from 'react-native';
+import {Provider} from 'react-redux';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>App</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
